9d001eaacf9a594d3b0d0abb7695abe08b1c3b45
Celio NELSON <celio.nelson@icloud.com>
2015-02-06 10:56:51 +0100

    First try for Biography Quiz from lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,48 @@
-var name = "Celio Nelson";
-var role = "Web Developer";
+var bio = {
+	"name" : "Celio NELSON",
+	"role" : "Web Developer",
+	"contact" : {
+		"mobile" : "+336 4740 7660",
+		"email" : "celio.nelson@icloud.com",
+		"github" : "celionelson",
+		"linkedin" : "link with logo", /* "https://www.linkedin.com/pub/celio-nelson/54/689/4b0/fr", */
+		"location" : "France"},
+	"bioPic" : "images/197x148.gif",
+	"welcomeMsg" : "Hey, guys! Welcome there",
+	"skills" : ["skill1", "skill2", "skill3"]
+ };
 
-var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contact.linkedin)
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg);
+var formattedSkill1 = HTMLskills.replace("%data%", bio.skills[0]);
+var formattedSkill2 = HTMLskills.replace("%data%", bio.skills[1]);
+var formattedSkill3 = HTMLskills.replace("%data%", bio.skills[2]);
 
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
+$("#topContacts").prepend(formattedLocation);
+$("#topContacts").prepend(formattedLinkedin);
+$("#topContacts").prepend(formattedGithub);
+$("#topContacts").prepend(formattedEmail);
+$("#topContacts").prepend(formattedMobile);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(HTMLskillsStart);
+$("#header").append(formattedSkill1);
+$("#header").append(formattedSkill2);
+$("#header").append(formattedSkill3);
+
+
+
+
+
 
 
